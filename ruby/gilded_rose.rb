7733ee77a87860f5129d95429674e41ce59507bc
Aditya Palepu <asanthosh@g2.com>
2024-08-01 21:27:09 +0530

    WIP

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -1,53 +1,111 @@
-class GildedRose
+# frozen_string_literal: true
 
+class NormalItems
+  attr_accessor :quality, :sell_in
+
+  def initialize(quality, sell_in)
+    @quality = quality
+    @sell_in = sell_in
+  end
+  def update_quality
+    return unless item.quality.positive? && item.quality <= 50
+    item.quality -= (!item.sell_in.positive? ? 2 : 1)
+  end
+end
+
+class AgedBrie
+  attr_accessor :quality, :sell_in
+
+  def initialize(quality, sell_in)
+    @quality = quality
+    @sell_in = sell_in
+  end
+  def update_quality
+    return unless item.quality >= 0 && item.quality < 50
+    item.quality += 1
+  end
+end
+
+class Sulfuras
+  attr_accessor :quality, :sell_in
+
+  def initialize(quality, sell_in)
+    @sell_in = sell_in
+    @quality = quality
+  end
+
+  def update_quality
+  end
+end
+
+class BackStagePasses
+  attr_accessor :quality, :sell_in
+
+  def initialize(quality, sell_in)
+    @sell_in = sell_in
+    @quality = quality
+  end
+
+  def update_quality
+    return unless item.quality >= 0 && item.quality <= 50
+
+    if !item.sell_in.positive?
+      item.quality = 0
+    elsif item.sell_in > 5 && item.sell_in < 11
+      item.quality += 2
+    elsif item.sell_in <= 5
+      item.quality += 3
+    else
+      item.quality += 1
+    end
+  end
+end
+
+# module QualityDegradation
+#   def self.normal_items(item)
+#     return unless item.quality.positive? && item.quality <= 50
+#
+#     item.quality -= (!item.sell_in.positive? ? 2 : 1)
+#   end
+#
+#   def self.aged_brie(item)
+#     return unless item.quality >= 0 && item.quality < 50
+#     item.quality += 1
+#   end
+#
+#   def self.sulfuras(_item); end
+#
+#   def self.backstage_passes(item)
+#     return unless item.quality >= 0 && item.quality <= 50
+#
+#     if !item.sell_in.positive?
+#       item.quality = 0
+#     elsif item.sell_in > 5 && item.sell_in < 11
+#       item.quality += 2
+#     elsif item.sell_in <= 5
+#       item.quality += 3
+#     else
+#       item.quality += 1
+#     end
+#   end
+# end
+
+class GildedRose
   def initialize(items)
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
+      case item.name
+      when 'Aged Brie'
+        AgedBrie.new(item.quality, item.sell_in)
+      when 'Backstage passes to a TAFKAL80ETC concert'
+        BackStagePasses.new(item.quality, item.sell_in)
+      when 'Sulfuras, Hand of Ragnaros'
+        Sulfuras.new(item.quality, item.sell_in)
       else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
+        NormalItems.new(item.quality, item.sell_in)
       end
     end
   end
--- ruby/gilded_rose_tests.rb
+++ ruby/gilded_rose_tests.rb
@@ -1,10 +1,73 @@
+# frozen_string_literal: true
+
 require 'minitest/autorun'
 require_relative 'gilded_rose'
 
 class TestGildedRose < Minitest::Test
-  def test_foo
-    items = [Item.new("foo", 0, 0)]
-    GildedRose.new(items).update_quality()
-    assert_equal "fixme", items[0].name
+  def test_quality_degradation_for_normal_items_everyday
+    items = [Item.new('foo', 10, 20)]
+    GildedRose.new(items).update_quality
+    assert_equal 19, items[0].quality
+  end
+
+  def test_quality_degradation_per_day_for_normal_items_past_sellin_date
+    items = [Item.new('foo', 0, 10)]
+    GildedRose.new(items).update_quality
+    assert_equal 8, items[0].quality
+  end
+
+  def test_positive_only_quality_for_all_items
+    sellin_days = 10
+    items = [Item.new('foo', 10, 2), Item.new('Aged Brie', 1, 1)]
+    sellin_days.times do
+      GildedRose.new(items).update_quality
+    end
+    assert_equal 0, items[0].quality
+  end
+
+  def test_aged_brie_quality_increase
+    items = [Item.new('Aged Brie', 1, 44)]
+    GildedRose.new(items).update_quality
+    assert_equal 45, items[0].quality
+  end
+
+  def test_updated_quality_less_than_50_for_all_items
+    items = [Item.new('Aged Brie', 1, 44), Item.new('Aged Brie', -1,50), Item.new('foo', 10, 2), Item.new('foo', -1, 47)]
+    GildedRose.new(items).update_quality
+    failed_items = items.select { |item| item.quality > 50 }
+    assert_empty failed_items
+  end
+
+  def test_constant_sulfuras_quality
+    original_quality = 80
+    items = [Item.new('Sulfuras, Hand of Ragnaros', 1, original_quality)]
+    GildedRose.new(items).update_quality
+    assert_equal original_quality, items[0].quality
+  end
+
+  def test_backstage_passes_quality_increases_by_2_when_sellin_days_between_6_and_10
+    # [6,10]
+    item_name = 'Backstage passes to a TAFKAL80ETC concert'
+    items = [Item.new(item_name, 10, 10), Item.new(item_name, 6, 4)]
+    GildedRose.new(items).update_quality
+    assert_equal 12, items[0].quality
+    assert_equal 6, items[1].quality
+  end
+  def test_backstage_passes_quality_becomes_zero_after_the_concert
+    items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 10)]
+    GildedRose.new(items).update_quality
+    assert_equal 0, items[0].quality
+  end
+  def test_backstage_passes_quality_increases_by_3_less_than_5_days_to_concert
+    items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 1, 10),Item.new('Backstage passes to a TAFKAL80ETC concert', 5, 0)]
+    GildedRose.new(items).update_quality
+    assert_equal 13, items[0].quality
+    assert_equal 3, items[1].quality
+  end
+
+  def test_backstage_passes_quality_increases_by_1_default
+    items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 100, 10)]
+    GildedRose.new(items).update_quality
+    assert_equal items[0].quality, 11
   end
 end
